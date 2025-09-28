9864dac5609d35e6f1eded152a5dc245ec32225f
Bruno Corona <brunocorona@Brunos-MacBook-Pro.local>
2025-09-27 20:39:46 -0600

    Import PokeApi

---
+++ packages/pokeapi/lib/domain/models/pokemon/pokemon.dart
@@ -0,0 +1,36 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'pokemon.g.dart';
+part 'pokemon.freezed.dart';
+
+@freezed
+abstract class Pokemon with _$Pokemon {
+  const factory Pokemon({
+    required int id,
+    required String name,
+    required String url,
+  }) = _Pokemon;
+
+  factory Pokemon.fromJson(Map<String, Object?> json) => _$PokemonFromJson(json);
+}
+
+extension PokemonX on Pokemon {
+  String get id => url.split('/').reversed.elementAt(1);
+  String get image => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/$id.png';
+}
+
+@freezed
+abstract class PokemonDetails with _$PokemonDetails {
+  const factory PokemonDetails({
+    required int id,
+    required String name,
+    required double height,
+    required double weight,
+  }) = _PokemonDetails;
+
+  factory PokemonDetails.fromJson(Map<String, Object?> json) => _$PokemonDetailsFromJson(json);
+}
+
+extension PokemonDetailsX on PokemonDetails {
+  String get image => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/$id.png';
+}
